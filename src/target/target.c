9498a16a341a38265f98bccc10524f519e7e44c9
Matthias Welwarsky <matthias.welwarsky@sysgo.com>
2016-05-19 11:52:10 +0200

    target: add verify_image_nodiff
    In a production test environment you sometimes only need to know if an
    image was uploaded correctly. A simple yes/no feedback is enough. Listing
    all the differences between source and upload takes very long for large
    images. The implemented command reuses the existing verification code
    and just skips bytewise diff if called as 'verify_image_nodiff'.
    
    Change-Id: I4abbd5dc5ec46aa0b963ec13b117a759fd93c45a
    Signed-off-by: Matthias Welwarsky <matthias.welwarsky@sysgo.com>

--- src/target/target.c
+++ src/target/target.c
@@ -3357,6 +3357,12 @@ static COMMAND_HELPER(handle_verify_image_command_internal, int verify)
 				/* failed crc checksum, fall back to a binary compare */
 				uint8_t *data;
 
+				if (verify != 1) {
+					LOG_ERROR("checksum mismatch");
+					retval = ERROR_FAIL;
+					goto done;
+				}
+
 				if (diffs == 0)
 					LOG_ERROR("checksum mismatch - attempting binary compare");
 
@@ -3420,6 +3426,11 @@ done:
 COMMAND_HANDLER(handle_verify_image_command)
 {
 	return CALL_COMMAND_HANDLER(handle_verify_image_command_internal, 1);
+}
+
+COMMAND_HANDLER(handle_verify_image_nodiff_command)
+{
+	return CALL_COMMAND_HANDLER(handle_verify_image_command_internal, 2);
 }
 
 COMMAND_HANDLER(handle_test_image_command)
@@ -6114,6 +6125,12 @@ static const struct command_registration target_exec_command_handlers[] = {
 	{
 		.name = "verify_image",
 		.handler = handle_verify_image_command,
+		.mode = COMMAND_EXEC,
+		.usage = "filename [offset [type]]",
+	},
+	{
+		.name = "verify_image_nodiff",
+		.handler = handle_verify_image_nodiff_command,
 		.mode = COMMAND_EXEC,
 		.usage = "filename [offset [type]]",
 	},
